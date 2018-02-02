3d24badcb7e9f544e1d710395013e56336506839
yykhoo88 <kennethkhoo.89@gmail.com>
2018-02-02 11:59:14 +0800

    ''

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,17 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinv <- function(inv) m <<- inv
+  getinv <- function() m
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
@@ -12,4 +22,20 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m <- x$getinv()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinv(m)
+  m
 }
+
+## This is the test suite, uncomment it to test
+#mat <- diag(1,2500,2500) #inverting this should take 10 sec
+#cachemat <- makeCacheMatrix()
+#cachemat$set(mat) #set in the matrix
+#cacheSolve(cachemat) #now solve it for the first time
+#cacheSolve(cachemat) #second time - this should be significantly faster
